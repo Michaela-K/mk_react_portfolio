f3053dcb819f07af11a37dbc04fbd0d29af84e99
Michaela-K <michaelakodes@gmail.com>
2024-01-10 10:16:54 -0500

    add about section

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+    <section id="About" className="about__section">
+      <div className="about__section__img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero__section__content__box about__section__box">
+        <div className="hero__section__content">
+          <p className="section__title">About</p>
+          <h1 className="skills-section__heading">About Me</h1>
+          <p className="hero__section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
+            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
+            tempora numquam sint odit optio.
+          </p>
+          <p className="hero__section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
+            recusandae?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/Home Page/index.jsx
+++ src/components/Home Page/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Hero'
 import Skills from '../Skills'
+import About from '../About'
 
 function Home() {
   return (
     <>
       <Hero/>
       <Skills/>
+      <About/>
     </>
   )
 }
