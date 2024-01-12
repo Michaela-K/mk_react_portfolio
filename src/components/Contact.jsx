487d23d1996667917a281a2c6df72cd73bb68414
Michaela-K <michaelakodes@gmail.com>
2024-01-12 09:16:15 -0500

    contact form works

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,51 @@
+import { useRef, useEffect, useState } from "react";
+import emailjs from '@emailjs/browser';
+
+const serviceID = process.env.REACT_APP_SERVICE_ID;
+const templateID = process.env.REACT_APP_TEMPLATE_ID;
+const userID = process.env.REACT_APP_USER_ID;
+
 export default function Contact() {
+const emailRef = useRef();
+const nameRef = useRef();
+const messageRef = useRef();
+const [emailSent, setEmailSent] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(serviceID, templateID, e.target, userID,{
+      user_name: nameRef.current.value,
+      user_email: emailRef.current.value,
+      message: messageRef.current.value,
+    })
+      .then((result) => {
+        setEmailSent(true);
+        console.log('Email sent successfully:', result);
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      });
+  };
+  
   return (
     <section id="Contact" className="contact__section">
       <div>
         <h2 className="heading">Contact Me</h2>
         <p className="sub__title">Let's Create Something together !</p>
       </div>
-      <form className="contact__form__container">
+      {emailSent ? (
+        <div className="sub__title success">Email sent successfully! Thank you for reaching out.</div>
+        ) : (
+      <form className="contact__form__container" onSubmit={sendEmail}>
         <div className="container">
           <label htmlFor="name" className="contact__label">
             <input
               type="text"
               className="contact__input text-md"
-              name="name"
+              name="user_name"
               id="name"
               placeholder="Full Name"
+              ref={nameRef}
               required
             />
           </label>
@@ -21,25 +53,31 @@ export default function Contact() {
             <input
               type="email"
               className="contact__input text-md"
-              name="email"
+              name="user_email"
               id="email"
               placeholder="Email"
+              ref={emailRef}
               required
             />
           </label>
         </div>
         <label htmlFor="message" className="contact__label">
-          <textarea
+          <input
+            type="text"
+            style={{padding: "16px 8px 145px 15px"}}
             className="contact__input text-md"
+            name="message"
             id="message"
             rows="8"
             placeholder="Type your message..."
+            ref={messageRef}
           />
         </label>
         <div>
           <button className="btn btn-primary contact__form__btn">Submit</button>
         </div>
       </form>
+      )}
     </section>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,9 @@ function Hero() {
     <section id="heroSection" className="hero__section">
       <div className="hero__section__content__box">
         <div className="hero__section__content">
-          <p id='spinning__text'></p>
           <p className="section__title">Michaela King</p>
           <h1 className="hero__section__title">
-            Web Developer
+          <span className="text-highlight">Web</span> Developer
           </h1>
           <p className="hero__section-description">
             Based in Ontario, Canada
