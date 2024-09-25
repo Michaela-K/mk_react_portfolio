449d0bf725900a4f22eb4a2c82832ea7f42d2cc0
Michaela-K <michaelakodes@gmail.com>
2024-09-25 11:17:13 -0400

    Add Hero img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import data from '../data/index.json'
 import { Link } from "react-scroll";
 
 function Hero() {
   return (
     <section id="heroSection" className="hero__section">
-      <div className="hero__section__content__box">
+      <div className="hero__section__content__box-left">
+        <div className="hero__section__img">
+          {data?.hero?.map((item, index) => (
+              <img src={item.src} alt="Placeholder"></img>
+            )
+          )}
+        </div>
+      </div>
+      <div className="hero__section__content__box-right">
         <div className="hero__section__content">
-          <p className="section__title">Michaela King</p>
+          <p className="section__label">Hi, my name is</p>
           <h1 className="hero__section__title">
-          <span className="text-highlight">Software</span> Developer
+            Michaela King
           </h1>
-          <p className="hero__section-description">
-            Based in Ontario, Canada
-          </p>
+          <p className="section__title">I'm a Software Developer</p>
+          <p className="section__title">based in Ontario, Canada</p>
         </div>
         <Link spy={true} smooth={true} duration={500} offset={-70} to="Contact">
           <button className="btn btn-primary">
@@ -20,6 +28,7 @@ function Hero() {
           </button>
         </Link>
       </div>
+        
     </section>
   )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,7 +12,7 @@ export default function Skills() {
             <div className="skills__section__card__content">
               <h3 className="skills__section__title">{item.title}</h3>
               <p className="skills__section__description">{item.description}</p>
-              <p className="text-md portfolio__link">
+              <p className="text-xl portfolio__link">
                 <a href={item.webflowref} target="_blank" rel="noopener noreferrer">
                   {item.externalLink}
                 </a>
