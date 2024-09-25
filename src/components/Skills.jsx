f13dd97e3d8de6249f6f1da6a7c9447f290abdc2
Michaela-K <michaelakodes@gmail.com>
2024-09-25 09:29:19 -0400

    Add link to webflow portfolio

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,6 +12,11 @@ export default function Skills() {
             <div className="skills__section__card__content">
               <h3 className="skills__section__title">{item.title}</h3>
               <p className="skills__section__description">{item.description}</p>
+              <p className="text-md portfolio__link">
+                <a href={item.webflowref} target="_blank" rel="noopener noreferrer">
+                  {item.externalLink}
+                </a>
+              </p>
             </div>
           </div>
         ))}
